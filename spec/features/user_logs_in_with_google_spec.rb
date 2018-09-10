3a224d4d608359937c5ef1ac2126722b74a9624c
Seth Schwartz <sschwartz214@gmail.com>
2018-09-10 13:53:07 -0600

    adds test for logging out

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -13,4 +13,17 @@ RSpec.feature "user logs in" do
     expect(page).to have_content("Seth Schwartz")
     expect(page).to have_link("Logout")
   end
+
+  scenario 'user can log out' do
+    stub_omniauth
+
+    visit root_path
+
+    click_link "Sign in with Google"
+    click_on "Logout"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Sign in with Google")
+    expect(page).to_not have_content("Seth Schwartz")
+  end
 end
