cdc12ba1cbc3775137b7ee0ac6b74ff5d6a47df1
Seth Schwartz <sschwartz214@gmail.com>
2018-09-16 17:27:52 -0600

    removes unused code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,5 @@
 class DashboardController < ApplicationController
 
   def index
-    @read_books = current_user.books.where(user_books: {status: 0})
-    @currently_reading_books = current_user.books.where(user_books: {status: 1})
-    @want_to_read_books = current_user.books.where(user_books: {status: 2})
   end
 end
