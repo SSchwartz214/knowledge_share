7d99a8aa41a6d3f399c99f5fe076333443c63a37
Seth Schwartz <sschwartz214@gmail.com>
2018-09-19 11:31:31 -0600

    text message working with interpolation

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,7 +1,9 @@
 class RecommendationsController < ApplicationController
   def create
+    book = params[:book]
+    link = params[:link]
     number = params[:number]
-    message = "#{current_user.first_name} has invited you to read book!  For more infor visit book_path"
+    message = "#{current_user.first_name} has invited you to read #{book}!  For more info visit #{"https://fierce-journey-80426.herokuapp.com" + link}"
       TwilioTextMessenger.new(message, number).call
   end
 end
